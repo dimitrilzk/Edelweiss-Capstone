b84d7e294bfe569a1ed1062616552c1645267daa
dimitrilzk <dimitrilazuka95@gmail.com>
2023-02-01 16:11:48 +0100

    fix ordini

---
+++ Edelweiss/Controllers/OrdiniController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Edelweiss.Models;
+
+namespace Edelweiss.Controllers
+{
+    public class OrdiniController : Controller
+    {
+        private ModelDbContext db = new ModelDbContext();
+
+        // GET: Ordini
+        public ActionResult Index()
+        {
+            var ordini = db.Ordini.Include(o => o.Pacchetti);
+            return View(ordini.ToList());
+        }
+
+        // GET: Ordini/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ordini ordini = db.Ordini.Find(id);
+            if (ordini == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ordini);
+        }
+
+        // GET: Ordini/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdPacchetto = new SelectList(db.Pacchetti, "IdPacchetto", "Nome");
+            ViewBag.Prezzo = (db.Pacchetti, "IdPacchetto", "PrezzoEffettivo");
+            return View();
+        }
+
+        // POST: Ordini/Create
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdOrdine,Nome,Cognome,Email,Cellulare,NumeroCarta,Scadenza,CodiceCVV,IdPacchetto,PrezzoAcquisto,NomePacchetto")] Ordini ordini)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ordini.Add(ordini);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdPacchetto = new SelectList(db.Pacchetti, "IdPacchetto", "Nome", ordini.IdPacchetto);
+            return View(ordini);
+        }
+
+        // GET: Ordini/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ordini ordini = db.Ordini.Find(id);
+            if (ordini == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdPacchetto = new SelectList(db.Pacchetti, "IdPacchetto", "Nome", ordini.IdPacchetto);
+            return View(ordini);
+        }
+
+        // POST: Ordini/Edit/5
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdOrdine,Nome,Cognome,Email,Cellulare,NumeroCarta,Scadenza,CodiceCVV,IdPacchetto,PrezzoAcquisto,NomePacchetto")] Ordini ordini)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ordini).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdPacchetto = new SelectList(db.Pacchetti, "IdPacchetto", "Nome", ordini.IdPacchetto);
+            return View(ordini);
+        }
+
+        // GET: Ordini/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ordini ordini = db.Ordini.Find(id);
+            if (ordini == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ordini);
+        }
+
+        // POST: Ordini/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ordini ordini = db.Ordini.Find(id);
+            db.Ordini.Remove(ordini);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
